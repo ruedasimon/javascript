c7c6759d4dee9856cc5d926340baf8359a35d0ab
mister rueda <ledyperez34@gmail.com>
2023-01-23 10:54:01 -0500

    updated2

--- a.js
+++ a.js
@@ -1,3 +1,11 @@
 const number1 = document.getElementById("number1");
 const number2 = document.getElementById("number2");
-const pid = document.getElementById("pid");
+const pid = document.getElementById("result");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", btnonclick);
+
+function btnonclick() {
+    const sumainputs = Number(number1.value) + Number(number2.value);
+    pid.innerText = "" + sumainputs;
+}
